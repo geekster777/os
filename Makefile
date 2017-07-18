954f1aeca9240c61a1a9dd56779e5c14cb3c7b73
Nick Hilton <nhilton777@gmail.com>
2017-07-18 16:31:28 -0400

    The bootloader now loads a method main from the floppy to run

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 BUILD_DIR=build
 BOOTLOADER=$(BUILD_DIR)/bootloader/bootloader.o
+OS=$(BUILD_DIR)/os/hello_world.o
 DISK_IMG=disk.img
 
 all: bootdisk
@@ -7,18 +8,21 @@ all: bootdisk
 .PHONY: bootdisk bootloader
 
 bootloader:
-	mkdir $(BUILD_DIR)
+	test -d $(BUILD_DIR) || mkdir $(BUILD_DIR)
 	make -C bootloader
+	make -C os
     
 bootdisk: bootloader
 	dd if=/dev/zero of=$(DISK_IMG) bs=512 count=2880
-	dd conv=notrunc if=$(BOOTLOADER) of=$(DISK_IMG) bs=512
+	dd conv=notrunc if=$(BOOTLOADER) of=$(DISK_IMG) bs=512 count=1 seek=0
+	dd conv=notrunc if=$(OS) of=$(DISK_IMG) bs=512 count=1 seek=1
 
 run:
-	qemu-system-i386 -machine q35 -fda $(DISK_IMG) -gdb tcp::260000
+	qemu-system-i386 -machine q35 -fda $(DISK_IMG) -gdb tcp::26000
 
 debug:
-	qemu-system-i386 -machine q35 -fda $(DISK_IMG) -gdb tcp::260000 -S
+	qemu-system-i386 -machine q35 -fda $(DISK_IMG) -gdb tcp::26000 -S
 
 clean:
 	make -C bootloader clean
+	make -C os clean
--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -2,77 +2,33 @@ org 0x7c00
 bits 16
 start: jmp boot
 
-; Simple hello world message to be displayed in the future
-msg db "Welcome to My Operating System!", 0ah, 0dh, 0h
-
 ; Basic boot process
 boot: 
     cli ; Clear interrupts
     cld ; Clear the direction flag
 
-    call ClearScreen
-
-    mov bh, 0x1
-    mov bl, 0x2
-    call MoveCursor
+    ; Set a buffer for the initial stack to sit above
+    mov ax, 0x50 
+    mov es, ax 
+    xor bx, bx
     
-    mov bh, 0x0
-    mov bl, 0x0
-    mov si, msg
-    call Print     
+    mov al, 2 ; Read from the floppy
+    mov ch, 0 ; Track 0
+    mov cl, 2 ; Second sector
+    mov dl, 0 ; Drive number
+    mov dh, 0 ; Head number
+    
+    mov ah, 0x02 ; Number of sectors to read
+    int 0x13     ; BIOS read from floppy
+
+    ; Move all the segment pointers
+    mov ax, 0x50
+    mov ds, ax
+    mov ss, ax
+    
+    jmp 0x500 ; Jump and execute the sector
 
     hlt 
-
-; Clear the screen by scrolling it up
-ClearScreen:
-    mov al, 0x0
-    mov bh, 0x0
-    mov cx, 0x0
-    mov dx, 0xffff
-    mov ah, 0x6
-    int 0x10
-    ret
-
-; Move the cursor to the x,y positions provided in bl,bh
-MoveCursor:
-    mov ah, 0x2 ; Interrupt number
-    mov dh, bh  ; Set row
-    mov dl, bl  ; Set column
-    mov bh, 0x0 ; Page 0
-    int 0x10
-    ret
-
-; Place a character on the screen in a selected color a number of times
-PutChar:
-    mov ah, 0x9 ; Interrupt number
-    mov bh, 0x0 ; Page 0
-    int 0x10
-    ret
-
-; Print a string
-Print:
-    mov  al, 0x0 ; Update cursor after writing
-    mov  bh, 0x0 ; Page 0
-    mov  bl, 0xb ; Color (cyan)
-    mov  ecx, 0x0 ; Initial character count
-    mov  bp, si  ; String pointer
-
-; Count the characters in the string
-_count_chars:
-    mov dl, byte[ebp+ecx]
-    cmp dl, 0x0
-    je _print_str
-    inc cx
-    jmp _count_chars
-
-; Restore the row, and call the print interrupt
-_print_str:
-    mov dx, 0x0303  ; Coordinates
-    mov ah, 0x13 ; Interrupt number
-    push ds
-    pop  es      ; String start segment  
-    int 0x10
-    ret
 
 ; Fill the excess space with zeros, and the boot signiture in the last 2 bytes
 times 510 - ($-$$) db 0
